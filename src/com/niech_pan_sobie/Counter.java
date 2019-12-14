ab7f2a741d1f3eebd89ce22f5a0e8e69e506ea20
nkorniluk <nkorniluk6@gmail.com>
2019-12-14 16:35:17 +0100

    [Student B] Changed puncation marks #26

--- src/com/niech_pan_sobie/Counter.java
+++ src/com/niech_pan_sobie/Counter.java
@@ -21,7 +21,7 @@ public class Counter {
             char[] chars = lines.get(0).toCharArray();
 
             for (char c: chars) {
-                if (c > 64 && c < 91 || c > 96 && c < 123) {
+                if (c == 46 || c == 63 ) {
                     lettersNumber++;
                 }
             }
