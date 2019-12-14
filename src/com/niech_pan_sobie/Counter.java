6b0b354352e6c41bfa2f6244071bb5520158af5b
nkorniluk <33404057+nkorniluk@users.noreply.github.com>
2019-12-14 13:51:23 +0100

    Count words function added (#17)

--- src/com/niech_pan_sobie/Counter.java
+++ src/com/niech_pan_sobie/Counter.java
@@ -1,30 +1,89 @@
 package com.niech_pan_sobie;
-
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Counter {
 
     public static void countLetters() {
-        Path path = Paths.get("exercise.txt");
+        final Path path = Paths.get("text.txt");
         try {
             int lettersNumber = 0;
-            List<String> lines = Files.readAllLines(path);
+            final List<String> lines = Files.readAllLines(path);
 
-            char[] chars = lines.get(0).toCharArray();
+            final char[] chars = lines.get(0).toCharArray();
 
-            for (char c: chars) {
+            for (final char c: chars) {
                 if (c > 64 && c < 91 || c > 96 && c < 123) {
                     lettersNumber++;
                 }
             }
 
             System.out.println("\n------------------------------\nNumber of letters in file: " + lettersNumber + "\n------------------------------\n");
-        } catch (IOException e) {
+        } catch (final IOException e) {
             System.out.println("\n----------------------------------------\nSorry, file has not yet been downloaded!\n----------------------------------------\n");
         }
+    }
+
+    public static void countWords() {
+        final Path path = Paths.get("text.txt");
+        int wordsNumber = 0;
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(path);
+        }  catch (final NoSuchFileException n) {
+            System.out.println("\n----------------------------------------\nSorry, file has not yet been downloaded!\n----------------------------------------\n");
+
+            return;
+        }
+        catch (final IOException e) {
+            return;
+        }
+        catch (final NullPointerException u) {
+            return;
+        }
+        while (scanner.hasNext()) {
+            scanner.next();
+            wordsNumber++;
+        }
+        scanner.close();
+        System.out.println("\n------------------------------\nNumber of words in file: " + wordsNumber + "\n------------------------------\n");
+    }
+
+    public static void generateReport() {
+        final Path path = Paths.get("text.txt");
+        try {
+            final Map<String, Integer> letters = generateAlphabetMap();
+            final List<String> lines = Files.readAllLines(path);
+            final String[] split = lines.get(0).split("");
+
+            for (final String s : split) {
+                if (letters.containsKey(s)) {
+                    int i = letters.get(s);
+                    letters.replace(s, ++i);
+                }
+            }
+            System.out.println("\n----");
+            letters.forEach((s, integer) -> System.out.println(s + ": " + integer));
+            System.out.println("----\n");
+        } catch (final IOException e) {
+            System.out.println("\n----------------------------------------\nSorry, file has not yet been downloaded!\n----------------------------------------\n");
+        }
+    }
+
+    private static Map<String, Integer> generateAlphabetMap() {
+        final Map<String, Integer> letters = new HashMap<>();
+        final String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+        for (final String s : alphabet) {
+            letters.put(s, 0);
+        }
+        return letters;
     }
 }
